806c936abef6102d19c056d2c394d5b338b1a94a
jossezs <zzss5224@163.com>
2024-04-28 23:41:43 +0800

    initialization commit

---
+++ leetcode/editor/cn/[7]整数反转.java
@@ -0,0 +1,17 @@
+package cn;
+
+/**
+ *  7-整数反转(reverse-integer)
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public static void main(String[] args) {
+        reverse(0);
+    }
+    public static int reverse(int x) {
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
